8fbcf1c97ddca3be4f7a73c64978776d5da3d07e
SDMckone <seandmckone@gmail.com>
2023-07-09 13:40:17 -0400

    Initial API code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
+const apiKey = "341cfdefb31e4a3591803936230807";
+
+const location = "London";
+
+async function getWeather() {
+  const response = await fetch(
+    `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}&days=7&aqi=no&alerts=no`
+  );
+
+  const data = await response.json();
+  console.log(data);
+}
+
+getWeather();
+
 console.log("hello");
