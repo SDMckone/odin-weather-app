63512e7bd159468a6d1af4120ff69e2c1af5907d
SDMckone <seandmckone@gmail.com>
2023-07-09 16:49:06 -0400

    Fix to include celsius temps

--- src/modules/weatherpanel.js
+++ src/modules/weatherpanel.js
@@ -2,11 +2,13 @@ class WeatherPanel {
   constructor(
     cityName,
     regionName,
-    currentTemp,
+    currentTempF,
+    currentTempC,
     currentCondition,
     currentRain,
     forecastDays,
-    forecastTemps,
+    forecastTempsF,
+    forecastTempsC,
     forecastRain,
     backgroundImage
   ) {
@@ -14,16 +16,20 @@ class WeatherPanel {
     this.cityName = cityName;
     // Name of this panel's region (state if US, country otherwise)
     this.regionName = regionName;
-    // This panel's current temp
-    this.currentTemp = currentTemp;
+    // This panel's current temp in Fahrenheit
+    this.currentTempF = currentTempF;
+    // This panel's current temp in Celsius
+    this.currentTempC = currentTempC;
     // This panel's current weather condition
     this.currentCondition = currentCondition;
     // This panel's current chance of rain
     this.currentRain = currentRain;
     // List of the next 7 days as 3 letter abbreviations
     this.forecastDays = forecastDays;
-    // List of average temperatures for the next 7 days
-    this.forecastTemps = forecastTemps;
+    // List of average temperatures for the next 7 days in Fahrenheit
+    this.forecastTempsF = forecastTempsF;
+    // List of average temperatures for the next 7 days in Celsius
+    this.forecastTempsC = forecastTempsC;
     // List of chances of rain for the next 7 days
     this.forecastRain = forecastRain;
     // Background image for this panel
