4233c1f30b1a8414161eaf72b0f54a8f1dabe523
SDMckone <seandmckone@gmail.com>
2023-07-10 18:54:02 -0400

    Fix api call to use HTTPS instead of HTTP

--- src/modules/getWeatherData.js
+++ src/modules/getWeatherData.js
@@ -1,6 +1,6 @@
 async function getWeatherData(apiKey, location) {
   const response = await fetch(
-    `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}&days=8&aqi=no&alerts=no`
+    `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}&days=8&aqi=no&alerts=no`
   );
 
   const data = await response.json();
